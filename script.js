a2492439b88ea04e42e92acd70fdcce46123b09c
lisandrofunes <lisandrofunes05@gmail.com>
2023-03-08 16:03:37 -0300

    add information of pomodoro

--- script.js
+++ script.js
@@ -167,3 +167,13 @@ function updateState(text){
         duration: 1000,
     });
 }
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
