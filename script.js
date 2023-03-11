84c6c3aa585fed13d850d486add061793d36cec7
Lisandro Funes <71905968+lisandrofunes@users.noreply.github.com>
2023-03-11 14:26:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ let circularProgress = document.querySelector(".circular-progress"),
     btnStart = document.querySelector(".button"),
     state = document.querySelector(".state"),
 
-    speed = 1000,
+    speed = 1,
     isPaused = false,
     isBreakTime = false,
     isUltimateBreakTime = false,
